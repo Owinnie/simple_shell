30200f5da8e9d481d8a3590bfd0a010b1f87b4b3
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-20 13:45:05 +0300

    e_prints

---
+++ e_prints.c
@@ -0,0 +1,117 @@
+#include "sshell.h"
+/**
+* print_s - prints a string
+* @s: string to be printed
+*
+* Return: number of characters printed
+*/
+int print_s(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+
+	return (i);
+}
+/**
+* prompt_user - prints $ to let user know the program is
+* ready to take their input
+* prints the prompt if the shell is in interactive mode
+* Return: no return
+*/
+void prompt_user(void)
+{
+	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
+		flags.interactive = 1;
+	if (flags.interactive)
+		write(STDERR_FILENO, "$ ", 2);
+}
+/**
+* main - carries out the read, execute then print output loop
+* @ac: argument count
+* @av: argument vector
+* @envp: environment vector
+*
+* Return: 0
+*/
+
+int main(int ac, char **av, char *envp[])
+{
+	char *line = NULL, *pathcommand = NULL, *path = NULL;
+	size_t bufsize = 0;
+	ssize_t linesize = 0;
+	char **command = NULL, **paths = NULL;
+	(void)envp, (void)av;
+	if (ac < 1)
+		return (-1);
+	signal(SIGINT, handle_signal);
+	while (1)
+	{
+		free_buffers(command);
+		free_buffers(paths);
+		free(pathcommand);
+		prompt_user();
+		linesize = getline(&line, &bufsize, stdin);
+		if (linesize < 0)
+			break;
+		info.ln_count++;
+		if (line[linesize - 1] == '\n')
+			line[linesize - 1] = '\0';
+		command = tokenizer(line);
+		if (command == NULL || *command == NULL || **command == '\0')
+			continue;
+		if (checker(command, line))
+			continue;
+		path = find_path();
+		paths = tokenizer(path);
+		pathcommand = test_path(paths, command[0]);
+		if (!pathcommand)
+			perror(av[0]);
+		else
+			execution(pathcommand, command);
+	}
+	if (linesize < 0 && flags.interactive)
+		write(STDERR_FILENO, "\n", 1);
+	free(line);
+	return (0);
+}
+/**
+* _strcmp - compares two strings
+* @s1: compared to s2;
+* @s2: compared to s1;
+*
+* Return: returns difference between strings
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, output;
+
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i) != '\0')
+		i++;
+
+	output = (*(s1 + i) - *(s2 + i));
+
+	return (output);
+}
+
+/**
+* _strlen - returns the length of a string
+* @s: string passed
+*
+* Return: returns length of string passed
+*/
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
