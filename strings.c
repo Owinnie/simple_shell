9dec6108e2f307a6dcc9e5e2c6caccd1ad215b84
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-20 13:47:46 +0300

    strings

---
+++ strings.c
@@ -0,0 +1,138 @@
+#include "sshell.h"
+/**
+* _strncmp - compares two strings up to n bytes
+* @s1: compared to s2
+* @s2: compared to s1
+* @n: number of bytes
+*
+* Return: difference between s1 and s2
+*/
+int _strncmp(char *s1, char *s2, int n)
+{
+	int i;
+
+	for (i = 0; s1[i] && s2[i] && i < n; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+/**
+* _strdup - dupicates string
+* @s: to be duplicated
+*
+* Return: pointer to duplicate string
+*/
+char *_strdup(char *s)
+{
+	char *ptr;
+	int i, len;
+
+	if (s == NULL)
+		return (NULL);
+
+	len = _strlen(s);
+
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return (NULL);
+	for (i = 0; *s != '\0'; s++, i++)
+		ptr[i] = s[0];
+
+	ptr[i++] = '\0';
+	return (ptr);
+}
+
+/**
+* _strchr - locates a character in a string
+* @s: string to be checked
+* @c: character to be located
+*
+* Return: returns pointer to first occurence of character
+* or NULL if character not found
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (!c)
+		return (s);
+	return (NULL);
+}
+/**
+* test_path - checks whether path is valid
+* @path: tokenized path
+* @command: user entered command
+*
+* Return: path appended with command on success
+* NULL on failure
+*/
+char *test_path(char **path, char *command)
+{
+	int i = 0;
+	char *output;
+
+	while (path[i])
+	{
+		output = append_path(path[i], command);
+		if (access(output, F_OK | X_OK) == 0)
+			return (output);
+		free(output);
+		i++;
+	}
+	return (NULL);
+}
+#include "shell.h"
+
+/**
+* tokenizer - creates tokens from given input
+* @line: to be tokenized
+*
+* Return: array of strings
+*/
+char **tokenizer(char *line)
+{
+	char *buf = NULL, *bufp = NULL, *token = NULL, *delim = " :\t\r\n";
+	char **tokens = NULL;
+	int tokensize = 1;
+	size_t index = 0, flag = 0;
+
+	buf = _strdup(line);
+	if (!buf)
+		return (NULL);
+	bufp = buf;
+
+	while (*bufp)
+	{
+		if (_strchr(delim, *bufp) != NULL && flag == 0)
+		{
+			tokensize++;
+			flag = 1;
+		}
+		else if (_strchr(delim, *bufp) == NULL && flag == 1)
+			flag = 0;
+		bufp++;
+	}
+	tokens = malloc(sizeof(char *) * (tokensize + 1));
+	token = strtok(buf, delim);
+	while (token)
+	{
+		tokens[index] = _strdup(token);
+		if (tokens[index] == NULL)
+		{
+			free(tokens);
+			return (NULL);
+		}
+		token = strtok(NULL, delim);
+		index++;
+	}
+	tokens[index] = '\0';
+	free(buf);
+	return (tokens);
+}
