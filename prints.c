33fdffd22bb1610995eb4e116e13fa1c9250b0f1
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-20 13:14:41 +0300

    prints

---
+++ prints.c
@@ -0,0 +1,82 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * _puts - prints a string
+ * @str: pointer to the string to print
+ * Return: void
+*/
+
+void _puts(char *str)
+{
+int i = 0;
+while (str[i])
+{
+	_putchar(str[i]);
+	i++;
+}
+}
+/**
+  * _strlen - find len of string
+  * @str: string to find len
+  *
+  * Return: 0, or len of string
+  */
+int _strlen(char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (0);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * *_strcpy -  copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest`
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
+/**
+ * *nbr_spaces - return the number of occurent of a string
+ * @s: string to check
+ * Return: int
+ */
+
+unsigned int nbr_spaces(char *s)
+{
+	int i, cmpt = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i]  == ' ')
+			cmpt++;
+	}
+
+	return (cmpt);
+}
