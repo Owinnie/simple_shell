1d20d297f8ecf253eda6e01c267908160632af0a
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-20 13:41:38 +0300

    prints

--- prints.c
+++ prints.c
@@ -1,82 +1,89 @@
 #include "sshell.h"
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+* handle_builtin - handles execution of builtin functions
+* @command: tokenized commands
+* @line: input read from stdin
+*
+* Return: 1 if executed, 0 if not
+*/
+int handle_builtin(char **command, char *line)
+{
+	struct builtin builtin = {"env", "exit"};
+
+	if (_strcmp(*command, builtin.env) == 0)
+	{
+		print_env();
+		return (1);
+	}
+	else if (_strcmp(*command, builtin.exit) == 0)
+	{
+		exit_cmd(command, line);
+		return (1);
+	}
+	return (0);
+}
+/**
+ *handle_signal- it keeps track is interactive mode
+ *@m: the signal number
+ *Return: nothing
  */
+
+void handle_signal(int m)
+{
+	(void)m;
+	write(STDERR_FILENO, "\n", 1);
+	write(STDERR_FILENO, "$ ", 2);
+}
+/**
+* find_path - finds the path from the global enviroment
+* Return: NULL if path is not found or path if path is found.
+*/
+char *find_path(void)
+{
+	int x;
+	char **env = environ, *path = NULL;
+
+	while (*env)
+	{
+		if (_strncmp(*env, "PATH=", 5) == 0)
+		{
+			path = *env;
+			while (*path && x < 5)
+			{
+				path++;
+				x++;
+			}
+			return (path);
+		}
+		env++;
+	}
+	return (NULL);
+}
+/**
+* print_env - prints the environment string to stdout
+*
+* Return: 0
+*/
+void print_env(void)
+{
+	int x = 0;
+	char **env = environ;
+
+	while (env[x])
+	{
+		write(STDOUT_FILENO, (const void *)env[x], _strlen(env[x]));
+		write(STDOUT_FILENO, "\n", 1);
+		x++;
+	}
+}
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned and errno set appropriately
+*/
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
-}
-/**
- * _puts - prints a string
- * @str: pointer to the string to print
- * Return: void
-*/
-
-void _puts(char *str)
-{
-int i = 0;
-while (str[i])
-{
-	_putchar(str[i]);
-	i++;
-}
-}
-/**
-  * _strlen - find len of string
-  * @str: string to find len
-  *
-  * Return: 0, or len of string
-  */
-int _strlen(const char *str)
-{
-	int i;
-
-	if (str == NULL)
-		return (0);
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	return (i);
-}
-/**
- * *_strcpy -  copies the string pointed to by src
- * @dest: char type string
- * @src: char type string
- * Description: Copy the string pointed to by pointer `src` to
- * the buffer pointed to by `dest`
- * Return: Pointer to `dest`
- */
-
-char *_strcpy(char *dest, const char *src)
-{
-	int i = -1;
-
-	do {
-		i++;
-		dest[i] = src[i];
-	} while (src[i] != '\0');
-
-	return (dest);
-}
-/**
- * *nbr_spaces - return the number of occurent of a string
- * @s: string to check
- * Return: int
- */
-
-unsigned int nbr_spaces(char *s)
-{
-	int i, cmpt = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i]  == ' ')
-			cmpt++;
-	}
-
-	return (cmpt);
 }
